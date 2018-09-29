fecf6a1a157ecdff847295ba73629eb9ff2e3be3
che6yp <che6yp@gmail.com>
2018-09-29 14:04:16 +0300

    Map generation base

---
+++ Assets/Scripts/Map/Chunk.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public GameObject solidBox;
+    public int[][] templ1 = new int[10][] 
+    {
+        new int[16] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[16] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    };
+
+    public void GenerateChunk(int tempId)
+    {
+
+        int[][] template = ChunkTemplates.templates[tempId];
+        for (int i = 0; i < template.Length; i++)
+        {
+            for (int k = 0; k < template[i].Length; k++)
+            {
+                SpawnBlock(k, i, template[i][k]);
+            }
+        }
+    }
+
+    void SpawnBlock(int x, int y, int type)
+    {
+        switch (type)
+        {
+            default:
+                break;
+            case 0:
+                break;
+            case 1:
+                GameObject b = Instantiate(solidBox, this.transform, false);
+                b.transform.localPosition = new Vector3(x, -y);
+                break;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    [Min(2)]
+    public int mapLength = 5;
+    public GameObject chunkPrefab;
+    public float chunkDistance = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateMap()
+    {
+
+        GameObject start_ch = Instantiate(chunkPrefab, transform, false);
+        start_ch.transform.localPosition = new Vector3(0, 4.5f);
+        start_ch.GetComponent<Chunk>().GenerateChunk(0);
+
+        for (int i = 1; i < mapLength-1; i++)
+        {
+            GameObject ch = Instantiate(chunkPrefab,  transform, false);
+            ch.transform.localPosition = new Vector3(i * chunkDistance, 4.5f);
+            ch.GetComponent<Chunk>().GenerateChunk(Random.Range(2, ChunkTemplates.templates.Length));
+        }
+
+        GameObject finish_ch = Instantiate(chunkPrefab, transform, false);
+        finish_ch.transform.localPosition = new Vector3((mapLength-1)*chunkDistance, 4.5f);
+        finish_ch.GetComponent<Chunk>().GenerateChunk(1);
+    }
+}
