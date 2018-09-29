b6a34c35849ff5d66d9621df6cdadd33ffa07cbd
che6yp <che6yp@gmail.com>
2018-09-29 19:33:44 +0300

    Chunk Editor

---
+++ Assets/Scripts/Map/MapGen/MapGenBlock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenBlock : MonoBehaviour
+{
+    public int blockType;
+    public SpriteRenderer spriteRenderer;
+
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBlockType(int type)
+    {
+        blockType = type;
+        if (type == 0)
+            spriteRenderer.sprite = null;
+        else
+            spriteRenderer.sprite = sprites[type - 1];
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+            SetBlockType(1);
+        else
+            if (Input.GetMouseButton(1))
+            SetBlockType(0);
+    }
+
+}
---
+++ Assets/Scripts/Map/MapGen/MapGenChunk.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenChunk : MonoBehaviour
+{
+    public GameObject mapGenBlock;
+
+    public int[][] emptyTemplate = new int[20][]
+    {
+        new int[32] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1  },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[32] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1  },
+    };
+
+    public MapGenBlock[][] mapGenBlocks = new MapGenBlock[20][];
+
+    
+
+
+    public void Start()
+    {
+        GenerateChunk();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            CopyTemplate();
+    }
+
+    public void GenerateChunk()
+    {
+
+        for (int i = 0; i < emptyTemplate.Length; i++)
+        {
+            mapGenBlocks[i] = new MapGenBlock[32];
+            for (int k = 0; k < emptyTemplate[i].Length; k++)
+            {
+                SpawnBlock(k, i + 1, emptyTemplate[i][k]);
+                //SpawnBlock(k, -(i + 1), emptyTemplate[i][k]);
+            }
+        }
+    }
+
+    void SpawnBlock(int x, int y, int type)
+    {
+        
+        GameObject b = Instantiate(mapGenBlock, this.transform, false);
+        b.transform.localPosition = new Vector3((float)x / 2, (float)-y / 2);
+        b.GetComponent<MapGenBlock>().SetBlockType(type);
+
+        mapGenBlocks[y-1][x] = b.GetComponent<MapGenBlock>();
+
+    }
+
+    int[][] GetTemplateMatrix()
+    {
+        int[][] newTemplate = new int[20][];
+
+        for (int i = 0; i < mapGenBlocks.Length; i++)
+        {
+            for (int k = 0; k < mapGenBlocks[i].Length; k++)
+            {
+                newTemplate[i][k] = mapGenBlocks[i][k].blockType;
+                //SpawnBlock(k, -(i + 1), emptyTemplate[i][k]);
+            }
+        }
+
+        return newTemplate;
+    }
+
+    string GetTemplateMatrixText()
+    {
+        string newTemplate = "new int[20][] { ";
+
+        for (int i = 0; i < mapGenBlocks.Length; i++)
+        {
+            //new int[32] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1  },
+            newTemplate += "new int[32] { ";
+            for (int k = 0; k < mapGenBlocks[i].Length; k++)
+            {
+                newTemplate += mapGenBlocks[i][k].blockType.ToString();
+                newTemplate += (k == mapGenBlocks[i].Length - 1) ? "" : ",";
+                //SpawnBlock(k, -(i + 1), emptyTemplate[i][k]);
+            }
+            newTemplate += "}";
+            newTemplate +=  (i == mapGenBlocks.Length - 1) ? "" : ",";
+        }
+        newTemplate += "},";
+
+        return newTemplate;
+    }
+
+    public void CopyTemplate()
+    {
+        TextEditor te = new TextEditor();
+
+
+        te.text = GetTemplateMatrixText();
+        te.SelectAll();
+        te.Copy();
+    }
+
+    
+}
