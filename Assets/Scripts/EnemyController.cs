73c016f072bab938efa3fae68b2d0acfb95ff543
Vitaly <kuvini@gmail.com>
2018-09-29 22:33:04 +0500

    New Enemy movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,32 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : PhysicsObject
+public class EnemyController : MonoBehaviour
 {
     public GameObject player;
     public float speed = 2;
     public float dist = 0;
     public float pos = 4;
+    public float direction = -1f;
+    public Rigidbody2D rBody;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");        
+        player = GameObject.FindGameObjectWithTag("Player");
+        rBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         dist = Vector2.Distance(player.transform.position, transform.position);
-
+        rBody.velocity = new Vector2(speed * direction, rBody.velocity.y);
+        transform.localScale = new Vector3(direction, 1, 1);
+        
+        /*
         if (dist <= pos)
         {
-            if (dist <= 1)
-            {
-                transform.position = player.transform.position;
-            }
-            else
-            {
-                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-            }            
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);         
         }
+        */
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+            direction *= -1f;
     }
 }
