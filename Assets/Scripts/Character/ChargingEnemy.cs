586f55a7884b793f3eec41be19cb7f713ed7a73e
CHE6yp <che6yp@gmail.com>
2019-04-25 04:15:40 +0300

    Charging enemies NOW CAN CHARGE!

--- Assets/Scripts/Character/ChargingEnemy.cs
+++ Assets/Scripts/Character/ChargingEnemy.cs
@@ -8,11 +8,16 @@ public class ChargingEnemy : AI
     //Creature creature;
 
     
-    public float walkingDuration = 5;
+    public float walkingDuration = 3;
     public float standingDuration = 2;
     public bool standing = false;
     public float playerDetectionDistance = 3;
     public float timer = 0;
+
+    public bool detected = false;
+    public bool charging = false;
+    public float chargingDuration = 1;
+    public float attackDuration = 3;
 
     public override float DetectGround()
     {
@@ -22,63 +27,130 @@ public class ChargingEnemy : AI
 
         Vector2 obstacleCheckDirection = (goRight) ? Vector2.right : Vector2.left;
         RaycastHit2D obstacleInfo = Physics2D.Raycast(detectionPointCurrent.position, obstacleCheckDirection, obstacleDetectionDistance);
-        Debug.DrawRay(detectionPointCurrent.position, obstacleCheckDirection, Color.white);
+        Debug.DrawRay(detectionPointCurrent.position, obstacleCheckDirection*obstacleDetectionDistance, Color.white);
 
         Vector2 playerDetection = (goRight) ? Vector2.right : Vector2.left;
         RaycastHit2D playerCheckInfo = Physics2D.Raycast(detectionPointCurrent.position, playerDetection, playerDetectionDistance);
         Debug.DrawRay(detectionPointCurrent.position+new Vector3(0,0.05f,0), playerDetection*playerDetectionDistance, Color.red);
 
 
-        if (playerCheckInfo.collider != null && playerCheckInfo.collider.tag == "Player")
-            Debug.Log("Player detected");
-
-
-
         timer += Time.deltaTime;
-        if (!standing)
+        //player is detected by ray
+        if (detected)
         {
-            if (timer >= walkingDuration)
+            Debug.Log("Player detected");
+            
+            
+
+            //enemy isn't charging his attack anymore
+            if (!charging)
             {
-                standing = true;
-                timer = 0;
-                return 0;
+                //attack time is up
+                if (timer >= attackDuration)
+                {
+                    standing = true;
+                    detected = false;
+                    timer = 0;
+                    return 0;
+                }
+                else
+                //attack is still going on
+                {
+                    //Хммм, не могу понять, говнокод ли это?
+                    //Выглядит как говнокод...
+                    if ((!groundInfo.collider ||
+                        groundInfo.collider.tag == "Spike" ||
+                        //groundInfo.collider.tag == "Player" ||
+                        (obstacleInfo.collider != null &&
+                        (obstacleInfo.collider.tag != "Player" &&
+                        obstacleInfo.collider.tag != "MeleeRange"))) &&
+                        creature.walking.grounded)
+                    {
+                        goRight = !goRight;
+                        time = 0;
+                        detectionPointCurrent = (detectionPointCurrent == detectionPoint1) ? detectionPoint2 : detectionPoint1;
+                    }
+                    return (goRight) ? 3 : -3;
+                }
             }
             else
+            //enemy is still charging its attack
             {
-                //Хммм, не могу понять, говнокод ли это?
-                //Выглядит как говнокод...
-                if ((!groundInfo.collider ||
-                    groundInfo.collider.tag == "Spike" ||
-                    //groundInfo.collider.tag == "Player" ||
-                    (obstacleInfo.collider != null &&
-                    (obstacleInfo.collider.tag != "Player" &&
-                    obstacleInfo.collider.tag != "MeleeRange"))) &&
-                    creature.walking.grounded)
+                //oh wait, no, he stopped
+                if (timer >= chargingDuration)
                 {
-                    goRight = !goRight;
-                    time = 0;
-                    detectionPointCurrent = (detectionPointCurrent == detectionPoint1) ? detectionPoint2 : detectionPoint1;
+                    charging = false;
+                    timer = 0;
+                    return 0;
                 }
-                return (goRight) ? 1 : -1;
+                else
+                //still charging!
+                {
+                    return 0;
+                }
+
             }
+
 
         }
         else
+
+
+        //if player not detected
         {
-            if (timer >= standingDuration)
+            //this is the first frame player is detected
+            if (playerCheckInfo.collider != null && playerCheckInfo.collider.tag == "Player")
             {
-                standing = false;
+                detected = true;
+                charging = true;
                 timer = 0;
-                return (goRight) ? 1 : -1;
-            }
-            else
-            {
                 return 0;
             }
 
+            if (!standing)
+            {
+                if (timer >= walkingDuration)
+                {
+                    standing = true;
+                    timer = 0;
+                    return 0;
+                }
+                else
+                {
+                    //Хммм, не могу понять, говнокод ли это?
+                    //Выглядит как говнокод...
+                    if ((!groundInfo.collider ||
+                        groundInfo.collider.tag == "Spike" ||
+                        //groundInfo.collider.tag == "Player" ||
+                        (obstacleInfo.collider != null &&
+                        (obstacleInfo.collider.tag != "Player" &&
+                        obstacleInfo.collider.tag != "MeleeRange"))) &&
+                        creature.walking.grounded)
+                    {
+                        goRight = !goRight;
+                        time = 0;
+                        detectionPointCurrent = (detectionPointCurrent == detectionPoint1) ? detectionPoint2 : detectionPoint1;
+                    }
+                    return (goRight) ? 1 : -1;
+                }
+
+            }
+            else
+            {
+                if (timer >= standingDuration)
+                {
+                    standing = false;
+                    timer = 0;
+                    return (goRight) ? 1 : -1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+
         }
-
-
 
 
         
